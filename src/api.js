e870b384a441fcce9ade64c654b47808abc4df65
jleomorris <70540466+jleomorris@users.noreply.github.com>
2021-01-09 21:51:43 +0000

    Add api data

---
+++ src/api.js
@@ -0,0 +1,37 @@
+// Base URL
+const base_url = "https://api.rawg.io/api/";
+
+// Calculate Month
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+
+  if (month < 10) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
+};
+
+// Calculate Day
+const getCurrentDay = () => {
+  const day = new Date().getDate();
+
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+};
+
+// Current day/month/year
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+// Popular games
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+export const popularGamesUrl = () => `${base_url}${popular_games}`;
