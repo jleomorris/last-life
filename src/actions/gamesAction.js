4d40402fbcf8709001d9dee32e98e60c8da7bab2
jleomorris <70540466+jleomorris@users.noreply.github.com>
2021-01-15 01:23:25 +0000

    Add upcoming and new games requests, actions and reducers

--- src/actions/gamesAction.js
+++ src/actions/gamesAction.js
@@ -1,16 +1,20 @@
 import axios from "axios";
-import { popularGamesUrl } from "../api";
+import { popularGamesUrl, upcomingGamesUrl, newGamesUrl } from "../api";
 
 // Action Creator
 
 export const loadGames = () => async (dispatch) => {
   // Fetch axios
   const popularData = await axios.get(popularGamesUrl());
+  const newGamesData = await axios.get(newGamesUrl());
+  const upcomingData = await axios.get(upcomingGamesUrl());
 
   dispatch({
     type: "FETCH_GAMES",
     payload: {
       popular: popularData.data.results,
+      newGames: newGamesData.data.results,
+      upcoming: upcomingData.data.results,
     },
   });
 };
--- src/reducers/gamesReducer.js
+++ src/reducers/gamesReducer.js
@@ -8,7 +8,12 @@ const initialState = {
 const gamesReducer = (state = initialState, action) => {
   switch (action.type) {
     case "FETCH_GAMES":
-      return { ...state, popular: action.payload.popular };
+      return {
+        ...state,
+        popular: action.payload.popular,
+        newGames: action.payload.newGames,
+        upcoming: action.payload.upcoming,
+      };
     default:
       return { ...state };
   }
