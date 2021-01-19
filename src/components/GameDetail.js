5aae4d538a578d86244aee7a637d00130f9065c4
jleomorris <70540466+jleomorris@users.noreply.github.com>
2021-01-19 23:38:11 +0000

    Add platform logos

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -8,15 +8,42 @@ import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 // Util
 import { convertToSmallImage } from "../util";
+// Images
+import playstation from "../img/playstation.svg";
+import steam from "../img/steam.svg";
+import xbox from "../img/xbox.svg";
+import nintendo from "../img/nintendo.svg";
+import apple from "../img/apple.svg";
+import gamepad from "../img/gamepad.svg";
 
 const GameDetail = ({ pathId }) => {
-  console.log(pathId, typeof pathId);
-
   const history = useHistory();
   //Data
   const { screenshots, game, isLoading } = useSelector((state) => state.detail);
 
-  //Exit detail
+  const getPlatform = (platform) => {
+    switch (platform) {
+      case "Playstation 4":
+        return playstation;
+      case "Playstation 5":
+        return playstation;
+      case "Xbox One":
+        return xbox;
+      case "Xbox S":
+        return xbox;
+      case "Xbox S/X":
+        return xbox;
+      case "PC":
+        return steam;
+      case "Nintendo Switch":
+        return nintendo;
+      case "iOS":
+        return apple;
+      default:
+        return gamepad;
+    }
+  };
+
   const exitDetailHandler = (e) => {
     const element = e.target;
     console.log(element);
@@ -56,7 +83,12 @@ const GameDetail = ({ pathId }) => {
                   <h3>Platforms</h3>
                   <Platforms>
                     {game.platforms.map((data) => (
-                      <h3 key={data.platform.id}>{data.platform.name}</h3>
+                      <img
+                        src={getPlatform(data.platform.name)}
+                        key={data.platform.id}
+                        title={data.platform.name}
+                        alt={data.platform.name}
+                      ></img>
                     ))}
                   </Platforms>
                 </Info>
@@ -89,6 +121,8 @@ const CardShadow = styled(motion.div)`
   position: fixed;
   top: 0;
   left: 0;
+  z-index: 5;
+
   &::-webkit-scrollbar {
     width: 0.5rem;
   }
@@ -109,13 +143,13 @@ const CardTop = styled(motion.div)`
 const Detail = styled(motion.div)`
   width: 80%;
   border-radius: 1rem;
-  /* padding: 2rem 5rem; */
   margin: 2rem;
   background: white;
   position: absolute;
   left: 10%;
   color: black;
   overflow: hidden;
+  z-index: 10;
 
   img {
     width: 100%;
@@ -131,7 +165,7 @@ const Stats = styled(motion.div)`
   justify-content: space-between;
   width: 100%;
   padding: 0rem 2rem;
-  background: rgba(0, 0, 0, 0.2);
+  background: rgba(0, 0, 0, 0.4);
 
   h3,
   p {
@@ -146,13 +180,14 @@ const Info = styled(motion.div)`
 const Platforms = styled(motion.div)`
   display: flex;
   justify-content: space-evenly;
+  margin-bottom: 2rem;
 
   h3 {
     margin: 0rem 1rem;
   }
 
   img {
-    margin-left: 3rem;
+    margin: 0rem 1rem;
   }
 `;
 
@@ -170,6 +205,7 @@ const Media = styled(motion.div)`
     bottom: -150px;
     right: 100px;
     position: absolute;
+    box-shadow: 5px 5px 30px rgba(0, 0, 0, 0.6);
   }
 `;
 
