75d07a2d1d25ff4a4ae030444e6381297c6ba02a
jleomorris <70540466+jleomorris@users.noreply.github.com>
2021-01-19 01:29:38 +0000

    Updated Game styling

--- src/components/Game.js
+++ src/components/Game.js
@@ -11,43 +11,67 @@ import { Link } from "react-router-dom";
 const Game = ({ name, released, image, id }) => {
   // Load details
   const dispatch = useDispatch();
+
   const loadDetailHandler = () => {
+    document.body.style.overflow = "hidden";
     dispatch(loadDetail(id));
   };
 
   return (
-    <StyledGame onClick={loadDetailHandler}>
-      <Link to={`/game/${id}`}>
-        <h3>{name}</h3>
-        <p>{released}</p>
-        <img src={image} alt={name} />
-      </Link>
+    <StyledGame>
+      <div style={{ background: `url(${image})` }} onClick={loadDetailHandler}>
+        <Link to={`/game/${id}`}>
+          <div className="title">
+            <h3>{name}</h3>
+            <p>{released}</p>
+          </div>
+        </Link>
+      </div>
     </StyledGame>
   );
 };
 
 const StyledGame = styled(motion.div)`
-  min-height: 30vh;
+  min-height: 40vh;
   box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.2);
-  text-align: center;
-  border-radius: 1rem;
+  text-align: left;
   overflow: hidden;
   cursor: pointer;
-  /* background-color: rgba(255, 255, 255, 0.15); */
-  /* backdrop-filter: blur(5px); */
+
+  div {
+    height: 100%;
+    background-size: cover !important;
+    background-position: top !important;
+  }
 
   a {
     display: flex;
     flex-direction: column;
-    justify-content: space-between;
+    justify-content: flex-start;
     align-items: center;
     height: 100%;
+
+    .title {
+      text-align: left;
+      width: 100%;
+      padding: 1.5rem;
+
+      h3,
+      p {
+        width: 100%;
+        color: white;
+        padding: 0;
+        text-shadow: 0px 5px 10px black;
+        font-weight: 900;
+      }
+    }
   }
 
   img {
     width: 100%;
     height: 40vh;
     object-fit: cover;
+    z-index: -1;
   }
 `;
 
